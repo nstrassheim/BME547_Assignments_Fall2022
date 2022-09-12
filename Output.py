54424a6aa573d00fdd384fe4d01d7e6ed882695f
nstrassheim <112418290+nstrassheim@users.noreply.github.com>
2022-09-12 12:20:33 -0400

    Delete Output.py

--- Output.py
+++
@@ -1,7 +0,0 @@
-
-weight = 20 / 2.205
-dosage = weight * 30
-
-print("CORRECT DOSAGE")
-print("For a patient weighing {} kg,".format(weight))
-print("  the correct dosage is {} mg the first day".format(dosage))
